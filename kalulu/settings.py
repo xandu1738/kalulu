a3347607dc290467b807c16d9dc9eca7301e750c
xandu1738 <51375353+xandu1738@users.noreply.github.com>
2022-10-12 10:57:49 +0300

    Update settings.py

--- kalulu/settings.py
+++ kalulu/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w4*x@r10!_891k8cm)%1w1kze(wz51-uc^^od8=yx)5mg8%h9e'
+SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
